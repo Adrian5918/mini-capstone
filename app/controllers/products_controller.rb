9cac8c1010c60dbc3769d60346452f9ca1dc52e1
Adrian <aguerra_57@yahoo.com>
2021-06-17 21:29:20 -0500

    hardcoded destroy method

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -15,19 +15,28 @@ class ProductsController < ApplicationController
       name: params[:input_name],
       price: params[:input_price],
       image_url: params[:input_image_url],
-      description: params[:input_description])
-      product.save
+      description: params[:input_description]
+    )
+    product.save
     render json: product.as_json
   end
 
   def update
     the_id = params[:id]
-    product = product.find_by(id: the_id)
-    product.name = ""
-    product.price = ""
-    product.image_url = ""
-    product.description = ""
-    product.save
+      product = Product.find_by(id: the_id)
+      product.name =  params[:input_name],
+      product.price = params[:input_price],
+      product.image_url = params[:input_image_url],
+      product.description = params[:input_description],
+      product.save
     render json: product.as_json
+  end
+
+  def destroy
+    #find product and destroy it
+    the_id = params[:id]
+    product = Product.find_by(id: the_id)
+    product.destroy
+    render json: {message: "successfully destroyed"}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # show all products
   get "/products", controller: "products", action: "index"
+  # show a specific product
   get "/products/:id", controller: "products", action: "show"
+  #create a new product
   post "/products", controller: "products", action: "create"
-  patch "/products/:id" controller: "products", action: "update"
+  #update an existing product by overwritting data
+  patch "/products/:id", controller: "products", action: "update"
+  #delete an existing product
+  delete "/products/:id", controller: "products", action: "destroy"
 end
