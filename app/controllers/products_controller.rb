da5212914b09b1e25d6acae28a67147ccc50802d
Adrian <aguerra_57@yahoo.com>
2021-06-17 20:14:11 -0500

    get id number

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,12 @@
 class ProductsController < ApplicationController
   def index
-    product = Product.first
+    product = Product.all
     render json: product.as_json 
+  end
+
+  def show
+    the_id = params[:id]
+    product = Product.find_by(id: the_id)
+    render json: product.as_json
   end
 end
